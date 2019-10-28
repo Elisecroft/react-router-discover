16ec76d38ed492951530ca4d7c8a140e031df18d
Elise Guezel <elise.guezel@yahoo.fr>
2019-10-28 11:34:45 +0100

    Make app functional

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    render() {
+        return (
+            <div>
+              <p>Bienvenue sur l'accueil</p>
+            </div>
+        );
+    }
+}
+
+export default Product;
